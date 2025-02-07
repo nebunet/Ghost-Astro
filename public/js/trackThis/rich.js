36ef46ccc04fe2c15708dc8810332fb025484e70
Shrey719 <shrey436@proton.me>
2025-02-07 18:10:43 -0500

    track dis

---
+++ public/js/trackThis/rich.js
@@ -0,0 +1,286 @@
+let tbl4 = ["https://www.luxurycard.com/goldcard",
+
+
+    "https://www.americanexpress.com/us/credit-cards/card-application/apply/platinum-charge-card/26129-10-0?pmccode=137&intlink=US-Acq-Shop-Consumer-CardDetails-Platinum-Prospect-Apply-Platinum-Header#/",
+    
+    
+    "https://combatgent.com/collections/sale",
+    
+    
+    "https://www.dstld.com/shop/womens",
+    
+    
+    "https://www.jhilburn.com/catalog/keylooks",
+    
+    
+    "https://www.acorns.com/",
+    
+    
+    "https://bellagio.mgmresorts.com/en.html",
+    
+    
+    "https://www.lurssen.com/en/",
+    
+    
+    "https://www.perininavi.it/",
+    
+    
+    "https://www.sunseeker.com/en-GB/",
+    
+    
+    "https://www.ferragamo.com/shop/us/en",
+    
+    
+    "https://www.manoloblahnik.com/us/",
+    
+    
+    "https://us.jimmychoo.com/en/home",
+    
+    
+    "https://www.patek.com/en/home",
+    
+    
+    "https://www.bulgari.com/en-us/",
+    
+    
+    "https://www.graff.com/",
+    
+    
+    "https://www.zara.com/us/",
+    
+    
+    "https://www.bmw.com/en/index.html",
+    
+    
+    "https://www.versace.com/us/en-us/home/",
+    
+    
+    "https://www.marcjacobs.com/",
+    
+    
+    "https://www.elliman.com/hamptons",
+    
+    
+    "https://www.germanhorsecenter.com/dressage-horses.html",
+    
+    
+    "https://www.christiesrealestate.com/eng/sales/tca/private-island-type",
+    
+    
+    "https://www.harneyre.com/features/horse-property/",
+    
+    
+    "https://www.danielgale.com/horse-properties",
+    
+    
+    "https://www.horseproperties.net/properties/new+york",
+    
+    
+    "https://lesliegarfield.com/properties/new-york",
+    
+    
+    "https://www.halstead.com/sales/hamptons/properties/tennis/",
+    
+    
+    "https://www.sothebysrealty.com/eng/sales/beverly-hills-ca-usa",
+    
+    
+    "https://www.corcoran.com/hamptons",
+    
+    
+    "https://www.nobleblack.com/properties/sale",
+    
+    
+    "https://www.stribling.com/",
+    
+    
+    "https://www.bhsusa.com/the-hamptons",
+    
+    
+    "https://thepointsguy.com/",
+    
+    
+    "https://www.thestreet.com/",
+    
+    
+    "https://www.thisismoney.co.uk/money/index.html",
+    
+    
+    "https://www.kiplinger.com/",
+    
+    
+    "https://money.cnn.com/data/markets/",
+    
+    
+    "https://www.stockpile.com/",
+    
+    
+    "https://www.betterment.com/",
+    
+    
+    "https://www.wealthbase.com/",
+    
+    
+    "https://us.etrade.com/home",
+    
+    
+    "https://www.coinbase.com/",
+    
+    
+    "https://www.ally.com/",
+    
+    
+    "https://robinhood.com/",
+    
+    
+    "https://www.ml.com/",
+    
+    
+    "https://finance.yahoo.com/",
+    
+    
+    "https://www.investopedia.com/",
+    
+    
+    "https://www.fool.com/",
+    
+    
+    "https://hakkasan.com/",
+    
+    
+    "https://marqueeny.com/",
+    
+    
+    "https://www.kittycatklub.net/",
+    
+    
+    "https://mgmgrand.mgmresorts.com/en.html",
+    
+    
+    "https://www.caesarscasino.com/",
+    
+    
+    "https://www.tomorrowland.com/global/",
+    
+    
+    "https://electriczoo.com/",
+    
+    
+    "https://www.amnesia.es/allevents/0/en/all-amnesia-tickets.html",
+    
+    
+    "https://www.standardhotels.com/new-york/features/biergarten-nyc",
+    
+    
+    "https://phdlounge.com/",
+    
+    
+    "https://www.bwin.com/",
+    
+    
+    "https://www.bet365.com/en/",
+    
+    
+    "https://lavony.com/",
+    
+    
+    "https://taodowntown.com/",
+    
+    
+    "https://www.katerblau.de/",
+    
+    
+    "https://www.christensenyachts.com/",
+    
+    
+    "https://www.alexandermcqueen.com/us",
+    
+    
+    "https://brianatwood.com/",
+    
+    
+    "https://www.stuartweitzman.com/home/",
+    
+    
+    "https://www.ulysse-nardin.com/",
+    
+    
+    "https://www.blancpain.com/en",
+    
+    
+    "https://www.girard-perregaux.com/en",
+    
+    
+    "https://www.breguet.com/en",
+    
+    
+    "https://www.vacheron-constantin.com/en2/home.html",
+    
+    
+    "https://www.piaget.com/",
+    
+    
+    "https://www.vancleefarpels.com/us/en.html",
+    
+    
+    "https://www.harrywinston.com/en",
+    
+    
+    "https://www.bugatti.com/",
+    
+    
+    "https://www.rolex.com/",
+    
+    
+    "https://www.cartier.com/",
+    
+    
+    "https://us.burberry.com/",
+    
+    
+    "https://www.coach.com/",
+    
+    
+    "https://www.dolcegabbana.com/",
+    
+    
+    "https://www.armani.com/us/armanicom",
+    
+    
+    "https://www.tiffany.com/",
+    
+    
+    "https://www.fendi.com/us",
+    
+    
+    "https://www.dior.com/en_us",
+    
+    
+    "https://www.chanel.com/us/",
+    
+    
+    "https://www.hermes.com/us/en/",
+    
+    
+    "https://us.louisvuitton.com/eng-us/homepage",
+    
+    
+    "https://www.oscardelarenta.com/",
+    
+    
+    "https://www.jaguarusa.com/index.html",
+    
+    
+    "https://www.lexus.com/",
+    
+    
+    "https://www.porsche.com/usa/",
+    
+    
+    "https://www.ferrari.com/en-US",
+    
+    
+    "https://www.audiusa.com/",
+    
+    
+    "https://www.mbusa.com/en/home"]
